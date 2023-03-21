186fbc8ec93aa4376ee9c14327df78bf22bc0a92
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-03-21 15:10:42 -0400

    complete all

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers=function(arr)
+{
+    return (arr.slice(0,2))
+}
+const returnLastTwoDrivers=function(arr)
+{
+   return (arr.slice((arr.length-2),arr.length))
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    }
+  }
+ 
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler=  createFareMultiplier(3);
+  function selectDifferentDrivers(arr,returnDrivers)
+  {
+    return returnDrivers(arr)
+  }
